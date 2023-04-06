0f56e41143a8f299d07912789e429a1d0b3cb5ec
nATOr <gowluonade@gmail.com>
2023-04-06 00:15:07 -0500

    initalState

---
+++ src/store/users/slice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface User {
+	name: string;
+	email: string;
+	github: string;
+}
+export interface UserWithId extends User {
+	id: string;
+}
+const initialState: UserWithId[] = [
+	{
+		id: "1",
+		name: "John Doe",
+		email: "jonh@gmail.com",
+		github: "jonhdoe",
+	},
+	{
+		id: "2",
+		name: "Jane Doe",
+		email: "janedoe@gmail.com",
+		github: "janedoe",
+	},
+	{
+		id: "3",
+		name: "Darius Smith",
+		email: "dariussmith@gmail.com",
+		github: "dariussmith",
+	},
+];
+
+export const usersSlice = createSlice({
+	name: "users",
+	initialState, // [...initialState]
+	reducers: {},
+});
+
+export default usersSlice.reducer;
