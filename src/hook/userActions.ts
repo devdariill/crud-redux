4b2fcd68d010c7bb92e82c2bdc8b5bb76befd4a0
nATOr <gowluonade@gmail.com>
2023-04-06 01:45:09 -0500

    refactor-handlerDelelte 2comits

---
+++ src/hook/userActions.ts
@@ -0,0 +1,10 @@
+import { UserId, deleteUserById } from "../store/users/slice";
+import { useAppDispatch } from "./store";
+
+export const useUserActions = () => {
+	const dispatch = useAppDispatch();
+	const removeUser = (id: UserId) => {
+		dispatch(deleteUserById(id));
+	};
+	return { removeUser };
+};
